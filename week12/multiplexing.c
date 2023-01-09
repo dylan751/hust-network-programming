782caee632a0e843fcf48d2e72a4aedadd211b2e
Nguyen Duong <muoi07052001@gmail.com>
2023-01-09 08:52:06 +0700

    Update multiplexing Server

--- week12/multiplexing.c
+++ week12/multiplexing.c
@@ -32,15 +32,18 @@
  * @brief Đề bài
  * Viết 1 Server để:
  * Nhận kết nối và lưu các socket vào một mảng
- * Nhận dữ liệu từ một client 
+ * Nhận dữ liệu từ một client
  * In dữ liệu nhận được ra màn hình
  * Gửi dữ liệu đó đến tất cả các client còn lại (không gửi lại cho chính client vừa gửi)
  */
 
+#define MAX_CLIENT 1024
 #define MAX_CONN_NUM 1024
 #define INVALID_SOCKET -1
 typedef struct sockaddr_in SOCKADDR_IN;
 typedef struct sockaddr SOCKADDR;
+int g_client[MAX_CLIENT];
+int g_count = 0;
 
 int main()
 {
@@ -55,7 +58,7 @@ int main()
     bind(s, (SOCKADDR *)&saddr, sizeof(saddr));
     listen(s, 10);
 
-    fd_set *fdread;
+    fd_set fdread;
     while (0 == 0)
     {
         // Làm đồng thời việc accept và nhận dữ liệu -> dùng thăm dò
@@ -65,19 +68,41 @@ int main()
         FD_SET(s, &fdread); // Thêm socket s vào tập read
 
         // Thêm cả các socket đã kết nối vào fdread
+        for (int i = 0; i < g_count ; i++)
+        {
+            FD_SET(g_client[i], &fdread); // Thêm socket
+        }
 
         select(FD_SETSIZE, &fdread, NULL, NULL, NULL); // Tạm thời chỉ sử dụng tập read -> các tập khác để null
 
         if (FD_ISSET(s, &fdread))
         {
-
             // Chương trình treo ở select() cho đến khi có kết nối tới
             // Gọi accept sau khi đã có kết nối -> hàm accept sẽ ko bị treo nữa
             int tmp = accept(s, (SOCKADDR *)&caddr, &clen);
             printf("Accepted another client\n");
             // Lưu tmp lại để SELECT lần sau
+            g_client[g_count++] = tmp;
         }
 
         // Lặp để kiểm tra các socket khác
+        for (int i = 0; i < g_count; i++)
+        {
+            // Nếu có dữ liệu kết nối đến
+            if (FD_ISSET(g_client[i], &fdread))
+            {
+                char buffer[1024] = {0};
+                recv(g_client[i], buffer, sizeof(buffer), 0);
+
+                // Gửi buffer tới tất cả client khác
+                for (int j = 0; j < g_count; j++)
+                {
+                    if (g_client[j] != g_client[i])
+                    {
+                        send(g_client[j], buffer, strlen(buffer), 0);
+                    }
+                }
+            }
+        }
     }
 }
